7c4efadce3d1f1b18be25167c56c8ad1646de2e2
moling3650 <365024424@qq.com>
2016-04-17 00:07:34 +0800

    orm模块已完成
    models模块加入orm的测试

--- www/models.py
+++ www/models.py
@@ -8,49 +8,80 @@ import time, uuid
 from Fields import *
 from orm import Model
 
-# import functools
-# StringField = functools.partial(StringField, ddl='varchar(50)')
+import functools
+StringField = functools.partial(StringField, ddl='varchar(50)')
 
 def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
+# 定义用户类
 class User(Model):
     __table__ = 'users'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    email = StringField(ddl='varchar(50)')
-    password = StringField(ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id)
+    email = StringField()
+    password = StringField()
     admin = BooleanField()
-    name = StringField(ddl='varchar(50)')
+    name = StringField()
     image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
 
-# class Blog(Model):
-#     __table__ = 'blogs'
+# 定义博客类
+class Blog(Model):
+    __table__ = 'blogs'
 
-#     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-#     user_id = StringField(ddl='varchar(50)')
-#     user_name = StringField(ddl='varchar(50)')
-#     user_image = StringField(ddl='varchar(500)')
-#     name = StringField(ddl='varchar(50)')
-#     summary = StringField(ddl='varchar(200)')
-#     content = TextField()
-#     created_at = FloatField(default=time.time)
+    id = StringField(primary_key=True, default=next_id)
+    user_id = StringField()
+    user_name = StringField()
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField()
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
 
-# class Comment(Model):
-#     __table__ = 'comments'
+# 定义评论类
+class Comment(Model):
+    __table__ = 'comments'
 
-#     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-#     blog_id = StringField(ddl='varchar(50)')
-#     user_id = StringField(ddl='varchar(50)')
-#     user_name = StringField(ddl='varchar(50)')
-#     user_image = StringField(ddl='varchar(500)')
-#     content = TextField()
-#     created_at = FloatField(default=time.time)
+    id = StringField(primary_key=True, default=next_id)
+    blog_id = StringField()
+    user_id = StringField()
+    user_name = StringField()
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
 
 if __name__ == '__main__':
-    # 创建一个实例：
-    u = User(name='Moling', email='test@orm.org', password='pw')
-    print(u)
-    # 保存到数据库：
-    u.save()
+    import aiomysql, asyncio, orm
+    loop = asyncio.get_event_loop()
+
+
+    async def orm_test():
+
+        pool = await orm.create_pool(loop, user='simpleblog',
+                                        password='test', db='simpleblog')
+        #--------------------测试insert into语句---------------------
+        for i in range(10):
+            u = User(name='test%s'%str(i), email='test%s@orm.org'%str(i),
+                        password='pw', image='test.jpg')
+            await u.save(pool)
+        #--------------------测试select语句--------------------------
+        users = await User.findAll(pool, orderBy='email desc', limit=(0,5))
+        for user in users:
+            print(user.name)
+        #--------------------测试update语句--------------------------
+        user = users[0]
+        user.name = 'new name'
+        await user.update(pool)
+        pk = user[User.__primary_key__]
+        test_user = await User.find(pool, pk)
+        assert test_user[User.__primary_key__] == pk, 'found a wrong guy'
+        assert user.name == test_user.name, 'Update was failed'
+        #--------------------测试delete语句-------------------------
+        users = await User.findAll(pool, orderBy='name desc', limit=(0,10))
+        for user in users:
+            await user.remove(pool)
+
+    loop.run_until_complete(orm_test())
+
+
--- www/orm.py
+++ www/orm.py
@@ -6,7 +6,62 @@ Created on Tue Apr 12 09:39:51 2016
 """
 import logging
 logging.basicConfig(level=logging.INFO)
+
+import aiomysql, asyncio
 from Fields import *
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s args: %s' % (sql, str(args)))
+
+
+async def create_pool(loop, **kw):
+    # 该函数用于创建连接池
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),  # 默认定义host名字为localhost
+        port=kw.get('port', 3306),      # 默认定义mysql的默认端口是3306
+        user=kw['user'],                # user是通过关键字参数传进来的
+        password=kw['password'],        # 密码也是通过关键字参数传进来的
+        db=kw['db'],                    # 数据库名字
+        charset=kw.get('charset', 'utf8'),  # 默认数据库字符集是utf8
+        autocommit=kw.get('autocommit', True),  # 默认自动提交事务
+        maxsize=kw.get('maxsize', 10),      # 连接池最多同时处理10个请求
+        minsize=kw.get('minsize', 1),       # 连接池最少1个请求
+        loop=loop       # 传递消息循环对象loop用于异步执行
+    )
+    return __pool
+
+
+async def select(pool, sql, args=None, size=None):
+    log(sql, args)
+    async with pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(pool, sql, args, autocommit=True):
+    log(sql, args)
+
+    async with pool.acquire() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise e
+        return affected
 
 class ModelMetaclass(type):
 
@@ -19,40 +74,32 @@ class ModelMetaclass(type):
         logging.info('found model: %s (table: %s)' % (name, table))
 
         mappings = {}
-        fields = []
         primary_key = None
         for key, val in attrs.copy().items():
             if isinstance(val, Field):
-                # print("field: %s, pk: %s" %(key, val.primary_key))
                 logging.info('found mapping: %s ==> %s' % (key, val))
                 mappings[key] = attrs.pop(key)
                 if val.primary_key:
                     if primary_key:
                         raise KeyError('Duplicate primary key for field: %s' % key)
                     primary_key = key
-                else:
-                    fields.append(key)
 
         if not primary_key:
             raise KeyError('Primary key not found.')
 
-        escaped_fields = ['`%s`' % field for field in fields]
-        # print(escaped_fields)
-        # print("primary_key: %s" % str(primary_key))
-        # print("fields: %s" % str(fields))
-        print("mappings: %s" % str(mappings))
         attrs['__table__'] = table                   # 保存表名
         attrs['__mappings__'] = mappings             # 保存属性和列的映射关系
         attrs['__primary_key__'] = primary_key       # 主键属性名
-        attrs['__fields__'] = fields                 # 非主键属性名的列表
-        attrs['__select__'] = ''
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (table, ', '.join(escaped_fields), primary_key, ', '.join(['?'] * len(mappings)))
-        attrs['__update__'] = ''
-        attrs['__delete__'] = ''
+        #-----------------------默认SQL语句--------------------------
+        attrs['__select__'] = 'select * from `%s`' % (table)
+        attrs['__insert__'] = 'insert into `%s` (%s) values (%s)' % (table, ', '.join('`%s`'%f for f in mappings), ', '.join(['?'] * len(mappings)))
+        attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (table, ', '.join('`%s` = ?'%f for f in mappings), primary_key)
+        attrs['__delete__'] = 'delete from `%s` where `%s`= ?' % (table, primary_key)
 
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
+
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -65,9 +112,6 @@ class Model(dict, metaclass=ModelMetaclass):
     def __setattr__(self, attr, value):
         self[attr] = value
 
-    def getValue(self, key):
-        return getattr(self, key, None)
-
     def getValueOrDefault(self, key):
         value = getattr(self, key, None)
         if value is None:
@@ -78,20 +122,52 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
         return value
 
+    @classmethod
+    async def findAll(cls, loop, where=None, args=None, **kw):
+        ' find objects by where clause. '
+        sql = [cls.__select__]
+        if where:
+            sql.append('where %s' % (where))
+        if kw.get('orderBy') is not None:
+            sql.append('order by %s' % (kw['orderBy']))
+        limit = kw.get('limit')
+        if limit is not None:
+            if isinstance(limit, int):
+                sql.append('limit ?')
+                args = (limit,)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('limit ?, ?')
+                args = limit
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(loop, ' '.join(sql), args or ())
+        return [cls(**r) for r in rs]
 
-    def save(self):
-        args = list(map(self.getValueOrDefault, self.__fields__ + []))
+    @classmethod
+    async def find(cls, pool, pk):
+        ' find object by primary key. '
+        rs = await select(pool, '%s where `%s`= ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
 
-        print('SQL: %s' %self.__insert__)
-        print('args: %s' %str(args))
+        return cls(**rs[0]) if len(rs) else None
 
-        # rows = await execute(self.__insert__, args)  # 使用默认插入函数
-        # if rows != 1: # 插入失败就是rows!=1
-        #     logging.warn('failed to insert record: affected rows: %s' % rows)
+    async def save(self, pool):
+        args = list(map(self.getValueOrDefault, self.__mappings__))
+        rows = await execute(pool, self.__insert__, args) # 使用默认插入函数
+        if rows != 1: # 插入失败就是rows!=1
+             logging.warn('failed to insert record: affected rows: %s' % rows)
 
-    # @classmethod
-    # async def findALL(cls, where=None, args=None, **kw):
-    #     sql = cls.[__select__]
-    #     if where:
-    #         sql.append('where')
-    #         sql.append(where)
+    async def update(self, pool):
+        args = list(map(self.get, list(self.__mappings__) + [self.__primary_key__]))
+        rows = await execute(pool, self.__update__, args)
+        if rows != 1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self, pool):
+        args = [self.get(self.__primary_key__)]
+        rows = await execute(pool, self.__delete__, args)
+        if rows != 1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
+
+if __name__ == '__main__':
+    pass
